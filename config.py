a2c5a3db7e786aa39ff5faacb3583778f526ff51
Rakesh Chowdhury <rc202402@gmail.com>
2020-04-27 14:25:25 +0530

    Updated config

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
+
 # place any of your own overrides here.
 # see bombsquad_server for details on what you can override
 # examples (uncomment to use):
-# config['partyName'] = 'My Awesome Party'
+config['partyName'] = 'Rakesh\'s Bombsquad Server'
 # config['sessionType'] = 'teams'
-# config['maxPartySize'] = 6
-# config['port'] = 43209
+config['maxPartySize'] = 6
+config['port'] = int(os.getenv("PORT", 43209))
 # config['playlistCode'] = 1242
